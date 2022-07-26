92d66bd6affcacef593af0f56667b2a5c7dc77f3
Florencia Nicole <florencianicolemanzanos@gmail.com>
2022-07-26 20:17:59 -0300

    3er Desafio - Arrays

--- js/script.js
+++ js/script.js
@@ -1,31 +1,36 @@
-//Calcular interes de un monto
-let monto = parseFloat(prompt('¡Bienvenido/a! \nIngrese el monto que desea calcular'));
-let opcionCuotas = prompt('Seleccione en cuantas cuotas lo quiere abonar: \n1 - 1 cuota sin interés \n2 - 2 y 3 cuotas con 10% de interés \n3 - 6 cuotas con 18% de interés \n4 - 9 cuotas con 24% de interés \n5 - 12 cuotas con 5% de interés \n0 - SALIR ')
-
-while (opcionCuotas != '0') {
-    switch (opcionCuotas) {
-        case '1':
-            alert('El monto a pagar es $' + monto);
-            break;
-        case '2':
-            alert('El monto a pagar es $' + (suma = parseFloat(monto) + parseFloat(monto * 10 / 100)));
-            break;
-        case '3':
-            alert('El monto a pagar es $' + (suma = parseFloat(monto) + parseFloat(monto * 18 / 100)));
-            break;
-        case '4':
-            alert('El monto a pagar es $' + (suma = parseFloat(monto) + parseFloat(monto * 24 / 100)));
-            break;
-        case '5':
-            alert('El monto a pagar es $' + (suma = parseFloat(monto) + parseFloat(monto * 5 / 100)));
-            break;
-
-        default:
-            alert('Opción incorrecta');
-            break;
+//Desafio Arrays
+const carrito = ['Hierbas', 'Frutos rojos'];
+const variedades = [{
+        id: 'Frutilla',
+        precio: 250
+    },
+    {
+        id: 'Hierbas',
+        precio: 250
+    },
+    {
+        id: 'Rosa Mosqueta',
+        precio: 250
+    },
+    {
+        id: 'Limon y Jengibre',
+        precio: 250
+    },
+    {
+        id: 'Frutos rojos',
+        precio: 300
+    },
+    {
+        id: 'Frutos azules',
+        precio: 300
     }
-    monto = parseFloat(prompt('¡Bienvenido/a! \nIngrese el monto que desea calcular'));
-    opcionCuotas = prompt('Seleccione en cuantas cuotas lo quiere abonar: \n1 - 1 cuota sin interés \n2 - 2 y 3 cuotas con 10% de interés \n3 - 6 cuotas con 18% de interés \n4 - 9 cuotas con 24% de interés \n5 - 12 cuotas con 5% de interés \n0 - SALIR ')
-}
+]
+console.log(variedades)
 
-alert('Esperamos que le haya sido util, que tenga buen dia!')
+variedades.unshift({id: 'Manzanilla', precio: '200'})
+console.log(variedades)
+
+console.log(carrito.includes('Hierbas'))
+console.log(carrito.includes('Manzanilla'))
+
+
